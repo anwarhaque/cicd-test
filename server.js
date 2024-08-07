b853d9e7d8df65efca2f2d52cd87803489db23f0
Anwarul Haque <anwarul@b2cinfosolutions.com>
2024-08-07 21:03:52 +0530

    env setup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 5050
+const PORT = process.env.PORT
 
 
 const apiRouter = express.Router();
