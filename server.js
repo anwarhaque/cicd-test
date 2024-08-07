5b95dd3f90c4b3756bae9a64ced49c9ccfb4dd86
Anwarul Haque <anwarul@b2cinfosolutions.com>
2024-08-07 20:19:36 +0530

    cicd base path

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const app = express()
 
 const PORT = process.env.PORT
 
-app.get('/api', (req, res)=>{
+
+const apiRouter = express.Router();
+
+
+apiRouter.get('/api', (req, res)=>{
     try {
         return res.status(200).json({message:'api working'})
     } catch (error) {
         return res.status(500).json({message:error.message})
     }
 })
+
+
+app.use('/cicd', apiRouter);
 
 app.listen(PORT, ()=>{
     console.log(`server running on http://localhost:${PORT}`);
